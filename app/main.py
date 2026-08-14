1ad20a600b292ef0186cd8b0880c51895bac3103
Artem D <OkeNNV@gmail.com>
2026-08-15 00:00:39 +0300

    flake8 fix

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ def battle(knights_config: dict) -> dict:
     battle_instance = Battle(knights_config)
     return battle_instance.fight()
 
+
 KNIGHTS = {
     "lancelot": {
         "name": "Lancelot",
